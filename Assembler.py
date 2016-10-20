d9abc8e70d9319295bcb5af0edfd6335ace119e7
Ken Bonar <kenbonar@gmail.com>
2016-10-20 15:54:39 -0600

    Readme and Instructions
    Added Readme and expanded instruction sets

--- Assembler.py
+++ Assembler.py
@@ -15,73 +15,94 @@ s = ""
 
 s += "memory_initialization_radix= {};\n".format(mem_radix)
 s += "memory_initialization_vector={}\n".format(init_vector)
- #TODO add COE header
- #memset or something
 
 
 with open(assembly_input_file + ".txt") as file:
     for count, line in enumerate(file):
         content = line
         content2 = content.split()
+        if content is not '\n':
+            if content2[0].lower() == "reset": # reset is 0000
+            # no arguments: all 16 bits are 0
+                s += coe_form(str("{:0>16b}".format(int("0", 10))))
 
+            elif content2[0].lower() == "load": # load is 0001
+            # load reg data
+            # next 4 bits are register, next 8 trash, next 16 are mem address
+                s += '0001'
+                if(int(content2[1],16) <= int('0xf', 16)):
+                    s += coe_form(str("{:0>12b}".format(int(content2[1], 16))))
+                else:
+                    error_print(count, content2[1])
+                    os.system('pause')
+                    break
+                #TODO Finish load three instruction (bank)
 
-        if content2[0].lower() == "reset": # reset is 0000
-        # no arguments: all 16 bits are 0
-            s += '0000'
-            s += str("{:0>14b}".format(int(content2[1],16)))
+                if (int(content2[2], 16) <= int('0xff', 16)):
+                    s += coe_form(str("{:0>12b}".format(int(content2[1], 16))))
+                else:
+                    error_print(count, content2[2])
+                    os.system('pause')
+                    break
+                if(int(content2[3],16) <= int('0xffff', 16)):
+                    s += coe_form(str("{:0>16b}".format(int(content2[3], 16))))
+                else:
+                    error_print(count, content2[3])
+                    os.system('pause')
+                    break
 
-        elif content2[0].lower() == "load": # load is 0001
-        # load reg data
-        # next 4 bits are register, next 8 trash, next 16 are mem address
-            s += '0001'
-            if(int(content2[1],16) <= int('0xfff', 16)):
-                s += coe_form(str("{:0>12b}".format(int(content2[1], 16))))
-            else:
-                error_print(count, content2[1])
-                break
-            if(int(content2[2],16) <= int('0xffff', 16)):
-                s += coe_form(str("{:0>16b}".format(int(content2[2], 16))))
-            else:
-                error_print(count, content2[2])
-                break
+            # store is 0010 - 4 bits reg - 8 bits junk - 16 bits mem location
+            elif content2[0].lower() == "store":
+                s += '0010'
+                if(int(content2[1], 16) <= int('0xfff', 16)):
+                    s += str("{:0>12b}".format(int(content2[1],16)))
+                else:
+                    error_print(count, content2[1])
+                    os.system('pause')
+                    break
+                #TODO Finish third bank instuction add
 
-        # store is 0010 - 4 bits reg - 8 bits junk - 16 bits mem location
-        elif content2[0].lower() == "store":
-            s += '0010'
-            if(int(content2[1], 16) <= int('0xfff', 16)):
+                if(int(content2[2], 16) <= int('0xffff', 16)):
+                    s += coe_form(str("{:0>12}".format(int(content2[2], 16))))
+                else:
+                    error_print(count, content2[2])
+                    os.system('pause')
+                    break
+
+            # advance is 0011
+            elif content2[0].lower() == "advance":
+                s += '0011'
                 s += coe_form(str("{:0>12b}".format(int(content2[1],16))))
-            else:
-                error_print(count, content2[1])
-            if(int(content2[2], 16) <= int('oxffff', 16)):
-                s += coe_form(str("{:0>16}".format(int(content2[2], 16))))
-            else:
-                error_print(count, content2[2])
 
-        # advance is 0011
-        elif content2[0].lower() == "advance":
-            s += '0011'
-            s += str("{:0>16b}".format(int(content2[1],16)))
+            # increment is 0100
+            elif content2[0].lower() == "increment":
+                s +=  '0100'
+                s += coe_form("{:0>12b}".format(int(content2[1],16)))
 
-        # increment is 0100
-        elif content2[0].lower() == "increment":
-            s +=  '0100'
-            s += str("{:0>16b}".format(int(content2[1],16)))
+            # pause is 0101
+            elif content2[0].lower() == "pause":
+                s += '0101'
+                s += coe_form("{:0>12b}".format(int(content2[1],16)))
 
-        # pause is 0101
-        elif content2[0].lower() == "pause":
-            s += '0101'
-            s += str("{:0>16b}".format(int(content2[1],16)))
-
-        # add is 0110 - 4 bits reg - 4 bits reg
-        elif content2[0].lower() == "add":
-            s += '0110'
-            if(int(content2[1]) <= int('0xff')):
-                s += str(int(content2[1],16))
-            else:
-                error_print(count, content2[1])
-            s += str(int(content2[2],16))
-            s += '0000'
-
+            # add is 0110 - 6 bits reg - 6 bits reg
+            elif content2[0].lower() == "add":
+                s += '0110'
+                # argument 1 check
+                if(int(content2[1], 16) <= int('0xff', 16)):
+                    s += "{:0>6b}".format(int(content2[1],16))
+                else:
+                    error_print(count, content2[1])
+                    os.system('pause')
+                    break
+                # argument 2 check
+                if(int(content2[2], 16) <= int('0ff', 16)):
+                    s += coe_form("{:0>6b}".format(int(content2[2], 16)))
+                else:
+                    error_print(count, content2[2])
+                    os.system('pause')
+                    break
+        else:
+            continue
 
 
 output = open('output.coe', 'w+')
---
+++ readme.md
@@ -0,0 +1,23 @@
+Etch - a - Sketch FPGA
+=======================================
+
+
+I. Introduction 
+=======================================
+
+
+II. Assembler
+=======================================
+
+Instructions:
+
+Instructions are case insensitive. Arguments should be given as Hexadecimal values.
+
+Reset:
+Takes no arguments, and will always return 16'b0 as 	output.
+
+Load:
+Takes in 3 arguments. First argument is the register (4 bits 0xf max). Second argument is the specifid bank (8 bits 0xff max) Third argument is the memory location (16 bits 0xffff max)
+
+
+
